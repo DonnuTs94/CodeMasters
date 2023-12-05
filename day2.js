50eecfb9e7e32da3ac68d3f844c9a0e3266c26aa
DonnuTs69 <febrykurniawanhakim@gmail.com>
2023-12-05 15:56:01 +0700

    day 2

--- day2.js
+++ day2.js
@@ -1,15 +1,102 @@
-const isPrime = (n) => {
-  if (n <= 1) {
-    return false
-  }
+// const isPrime = (n) => {
+//   if (n <= 1) {
+//     return false
+//   }
 
-  for (let i = 2; i < n; i++) {
-    if (n % i == 0) {
-      return false
-    }
-  }
+//   for (let i = 2; i < n; i++) {
+//     if (n % i == 0) {
+//       return false
+//     }
+//   }
 
-  return true
-}
+//   return true
+// }
 
-console.log(isPrime(2))
+// console.log(isPrime(2))
+
+// =================================================================
+
+// const addTwoNumber = (a, b) => {
+//   return a + b
+// }
+
+// console.log(addTwoNumber(5, 3))
+
+// =================================================================
+
+// const evenOrOdd = (n) => {
+//   if (n % 2 == 0) {
+//     return "Genap"
+//   }
+//   return "ganjil"
+// }
+
+// console.log(evenOrOdd(3))
+
+// =================================================================
+
+// const names = ["Alice", "Bob", "Charlie", "David", "Eve"]
+
+// console.log("List of namess:", names)
+
+// names.push("Frank")
+// console.log("After adding 'frank':", names)
+
+// names.pop()
+// console.log("After removing the last names:", names)
+
+// const secondnames = names[1]
+// console.log("The second names in the list is:", secondnames)
+
+// const totalLength = names.length
+// console.log("Numbe of names in the list:", totalLength)
+
+// =================================================================
+
+// reverse String
+
+const str = "hello world"
+console.log(str.split("").reverse().join(""))
+
+// Fibonacci
+
+// const Fibonacci = (n) => {
+//   let a = 0
+//   let b = 1
+//   let z
+
+//   for (let i = 2; i < n; i++) {
+//     z = a + b
+//     a = b
+//     b = z
+//   }
+//   return z
+// }
+// console.log("The result is", Fibonacci(7))
+
+// ==========================================
+
+// const genFibonacci = (length) => {
+//   let fiboArr = [0, 1]
+//   for (let i = 2; i < length; i++) {
+//     fiboArr.push(fiboArr[i - 2] + fiboArr[i - 1])
+//   }
+//   return fiboArr
+// }
+
+// console.log(genFibonacci(8))
+
+// Palindrome
+
+// const palindrome = (str) => {
+//   let rgx = /[^A-Za-z]/g
+//   let lowCaseStr = str.toLowerCase().replace(rgx, "")
+//   let reverseStr = lowCaseStr.split("").reverse().join("")
+
+//   if (reverseStr === lowCaseStr) {
+//     return true
+//   }
+//   return false
+// }
+
+// console.log(palindrome("A man, a plan, a canal, Panama"))
---
+++ tugas1.js
@@ -0,0 +1,86 @@
+// Soal 1
+
+const firstName = "John"
+const lastName = "Doe"
+
+// console.log(firstName, lastName)
+
+// Soal 2
+
+const age = 30
+// console.log(typeof age)
+
+const isStudent = true
+// console.log(typeof isStudent)
+
+// Soal 3
+
+const num1 = 10
+const num2 = 20
+
+let sum = num1 + num2
+// console.log(sum)
+
+let min = num1 - num2
+// console.log(min)
+
+let multiplication = num1 * num2
+// console.log(multiplication)
+
+let distribution = num1 / num2
+// console.log(distribution)
+
+// Soal 4
+
+const umur = 18
+if (umur >= 18) {
+  //   console.log("Anda sudah dewasa")
+} else {
+  //   console.log("Anda belum dewasa")
+}
+
+// Soal 5
+
+for (let i = 1; i <= 10; i++) {
+  //   console.log(i, "For Loop")
+}
+
+let i = 10
+while (i > 0) {
+  //   console.log(i, "While Loop")
+  i--
+}
+
+let k = 1
+do {
+  //   console.log(k, "Do While Loop")
+  k++
+} while (k <= 10)
+
+// Soal 6
+
+const greed = (name) => {
+  return name
+}
+
+// console.log(greed("John"))
+
+const calculatedArea = (width, height) => {
+  return width * height
+}
+
+// console.log(calculatedArea(10, 5))
+
+// Soal 7
+const fruits = ["apple", "banana", "orange"]
+// console.log(fruits[0])
+
+fruits[1] = "grape"
+fruits.push("mango")
+// console.log(fruits)
+
+for (let j = 0; j < fruits.length; j++) {
+  console.log(fruits[j])
+}
+
+// https://gist.github.com
