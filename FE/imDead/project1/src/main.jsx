03028c3ec639560b5251d81d1e00a2af2f4f6c17
DonnuTs69 <febrykurniawanhakim@gmail.com>
2024-01-04 09:09:37 +0700

    fak

--- FE/imDead/project1/src/main.jsx
+++ FE/imDead/project1/src/main.jsx
@@ -5,6 +5,7 @@ import App from "./App.jsx"
 import { ChakraProvider } from "@chakra-ui/react"
 import { RouterProvider } from "react-router-dom"
 import { router } from "./routes.jsx"
+import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
